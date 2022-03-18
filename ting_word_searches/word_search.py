77ff5d0457127398c4cccc471ba3c3c26c7bae49
Southier <patrick.southier@hotmail.com>
2022-03-18 02:56:01 -0300

    feat: creating the INCOMPLETE exists_word function at word_search file. Having troubles with the logics

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    search_result = list()
+    for index in range(len(instance)):
+        curr_file = instance.search(index)
+        search_data = {}
+        search_data["palavra"] = word
+        search_data["arquivo"] = curr_file["nome_do_arquivo"]
+        search_data["ocorrencias"] = list()
+        for line_index in range(curr_file["qtd_linhas"]):
+            if word in curr_file["linhas_do_arquivo"][line_index].split(" "):
+                search_data["ocorrencias"].append({"linha": line_index})
+    #     line_words = curr_file["linhas_do_arquivo"][line_index].split(" ")
+    #     for curr_word in line_words:
+    #         if curr_word == word:
+    #             search_data["ocorrencias"].append({"linha": line_index})
+        if len(search_data["ocorrencias"]) > 0:
+            search_result.append(search_data)
+    return search_result
 
 
 def search_by_word(word, instance):
