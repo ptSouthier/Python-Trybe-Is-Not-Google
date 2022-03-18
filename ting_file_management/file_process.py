e080e6a9ce15dbdc8464f5655e83f55092b378f7
Southier <patrick.southier@hotmail.com>
2022-03-18 01:44:21 -0300

    feat: creating the process function at file_process file

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,18 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for i in range(len(instance)):
+        if instance.search(i)["nome_do_arquivo"] == path_file:
+            return None
+    path_file_content = txt_importer(path_file)
+    processed_content = {}
+    processed_content["nome_do_arquivo"] = path_file
+    processed_content["qtd_linhas"] = len(path_file_content)
+    processed_content["linhas_do_arquivo"] = path_file_content
+    instance.enqueue(processed_content)
+    return sys.stdout.write(str(processed_content))
 
 
 def remove(instance):
